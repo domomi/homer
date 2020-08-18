865ef24635addaa64d3f09c210cc8b1d886c1251
Rickson2019 <2528889836@qq.com>
2020-08-18 10:13:18 -0700

    Changed the folder name to homer-lite

--- SimplifiedVersion/homer-simplified/src/redux/index.js
+++
@@ -1,9 +0,0 @@
-//  The resulting reducer calls every child reducer, 
-//  and gathers their results into a single state object.
-import { combineReducers } from 'redux'
-
-import reducer from './reducers/reducer'
-
-export default combineReducers({
-    global_data : reducer
- })
